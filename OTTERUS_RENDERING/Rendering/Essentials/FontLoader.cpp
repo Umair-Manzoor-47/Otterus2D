41124d702afc81519550d5ccc1c27cfd83d31bc7
Umair Manzoor <umairmanzoor546@gmail.com>
2026-08-22 18:08:03 +0500

    wip(font-loading): add font loader function

--- OTTERUS_RENDERING/Rendering/Essentials/FontLoader.cpp
+++ OTTERUS_RENDERING/Rendering/Essentials/FontLoader.cpp
@@ -1,14 +1,53 @@
 #include "FontLoader.h"
 #include "Font.h"
+#include <fstream>
+#include <vector>
+#include <logger/Logger.h>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <SOIL/stb_truetype.h>
 
 namespace otterus_rendering {
 
-    std::shared_ptr<Font> FontLoader::Create(const std::string& fontPath, float fontSize, int width, int heigt)
+    std::shared_ptr<Font> FontLoader::Create(const std::string& fontPath, float fontSize, int width, int height)
     {
-        return std::shared_ptr<Font>();
+        std::ifstream fontStream{ fontPath, std::ios::binary };
+
+        if (fontStream.fail()) 
+        {
+            OTTERUS_ERROR("Failed to load font [{}] - Unable to read buffer!", fontPath);
+            return nullptr;
+        }
+
+        fontStream.seekg(0, fontStream.end);
+        int64_t length = fontStream.tellg();
+        fontStream.seekg(0, fontStream.beg);
+
+        std::vector<unsigned char> buffer;
+        buffer.resize(length);
+        std::vector<unsigned char> bitmap;
+        bitmap.resize(width * height);
+        fontStream.read((char*)(&buffer[0]), length);
+        auto data = (void*) new stbtt_bakedchar[96];
+        
+        stbtt_BakeFontBitmap(buffer.data(), 0, fontSize, bitmap.data(), width, height, 32, 96, (stbtt_bakedchar*)(data));
+
+        GLuint fontID;
+        glGenTextures(1, &fontID);
+        
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, bitmap.data());
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+        return std::make_shared<Font>(fontID, width, height,fontSize, data);
     }
 
-    std::shared_ptr<Font> FontLoader::CreateFromMemory(const unsigned char* fontPath, float fontSize, int width, int heigt)
+    std::shared_ptr<Font> FontLoader::CreateFromMemory(const unsigned char* fontPath, float fontSize, int width, int height)
     {
         return std::shared_ptr<Font>();
     }
--- OTTERUS_RENDERING/Rendering/Essentials/FontLoader.h
+++ OTTERUS_RENDERING/Rendering/Essentials/FontLoader.h
@@ -10,8 +10,8 @@ namespace otterus_rendering {
 	public:
 		FontLoader() = delete;
 
-		static std::shared_ptr<class Font> Create(const std::string& fontPath, float fontSize = 32.f, int width = 512, int heigt = 512);
-		static std::shared_ptr<class Font> CreateFromMemory(const unsigned char* fontPath, float fontSize = 32.f, int width = 512, int heigt = 512);
+		static std::shared_ptr<class Font> Create(const std::string& fontPath, float fontSize = 32.f, int width = 512, int height = 512);
+		static std::shared_ptr<class Font> CreateFromMemory(const unsigned char* fontPath, float fontSize = 32.f, int width = 512, int height = 512);
 
 
 
